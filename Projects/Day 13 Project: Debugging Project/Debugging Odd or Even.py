f09da685cc90aff4754e31fd94382b6ea1ad6193
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2024-11-12 23:34:26 +0000

    Debugging Odd or Even.py

---
+++ Projects/Day 13 Project: Debugging Project/Debugging Odd or Even.py
@@ -0,0 +1,24 @@
+# Debugging Odd or Even
+
+"""
+- Read the code in the exercise.py file.
+- Find a problem 🐞.
+- Fix the problem by modifying the program.
+
+Modify the code to operate on submission and to pass the test.
+You can copy and paste the code to PyCharm to help with debugging.
+"""
+
+# Original code
+def odd_or_even(number):
+    if number % 2 = 0:
+        return "This is an even number."
+    else:
+        return "This is an odd number."
+        
+# Debugged code
+def odd_or_even(number):
+    if number % 2 == 0:
+        return "This is an even number."
+    else:
+        return "This is an odd number."
