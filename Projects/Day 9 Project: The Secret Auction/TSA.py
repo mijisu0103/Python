a285b2fc310d49d192eb8aacb3b5937811c62fca
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2024-11-12 21:45:00 +0000

    TSA.py

---
+++ Projects/Day 9 Project: The Secret Auction/TSA.py
@@ -0,0 +1,33 @@
+# TSA.py
+# TODO-1: Ask the user for input
+# TODO-2: Save data into dictionary {name: price}
+# TODO-3: Whether if new bids need to be added
+# TODO-4: Compare bids in dictionary
+
+import art
+print(art.logo)
+
+def find_highest_bidder(bidding_dictionary):
+    winner = ""
+    highest_bid = 0
+    for bidder in bidding_dictionary:
+        bid_amount = bidding_dictionary[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+bids = {}
+continue_bidding = True
+while continue_bidding:
+    name = input("What is your name?: ")
+    price = int(input("What is your bid? : $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no'. \n").lower()
+
+    if should_continue == "no":
+        continue_bidding = False
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        print("\n" * 100)
