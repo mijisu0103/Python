2f94895bd1bf880abc8bbcde441d45f54374ea90
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2024-11-17 01:40:15 +0000

    TNGG.py

--- Projects/Day 12 Project: The Number Guessing Game/TNGG.py
+++ Projects/Day 12 Project: The Number Guessing Game/TNGG.py
@@ -33,7 +33,7 @@ def game():
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100.")
     answer = randint(1, 100)
-    print(f"Pssst, the correct answer is {answer}")
+    # print(f"Pssst, the correct answer is {answer}") # Testing Purpose
 
     turns = set_difficulty()
 
